2bae81c1d8fb2686de291c7f527d704a032b7241
SrNok12 <00001141118075sp@al.educacao.sp.gov.br>
2024-11-29 14:45:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'O que é fotossíntese?',
+    'É o processo pelo qual as plantas, algas e algumas bactérias convertem luz solar, água e dióxido de carbono em glicose e oxigênio.'
+)
+
+criaCartao(
+    'Historia',
+    'Em que ano ocorreu a Proclamação da República no Brasil?',
+    'A Proclamação da República no Brasil ocorreu em 1889.'
+)
+
+criaCartao(
+    'Matematica',
+    'Quantos lados tem um hexágono?',
+    'Um hexágono tem seis lados.'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual é a fórmula química da água?',
+    'A fórmula química da água é H₂O.'
+)
